132106bd9ff9b9a503ffed7bfc1cdf7ea663e006
OlegPlugaru <olegplugaru.md@gmail.com>
2024-02-26 19:41:16 +0200

    Initial Commit

---
+++ FirebaseSPM/FirebaseSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSPMApp.swift
+//  FirebaseSPM
+//
+//  Created by Oleg Plugaru on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
